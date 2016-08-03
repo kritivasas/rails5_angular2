84648d8d8b87bbbafd7296f7ee66e0923db6026b
vineetsouri <vineetsouri72@gmail.com>
2016-08-03 16:13:05 +0530

    generated rails api tasks controler and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def dot
+  print '.'
+end
+
+def seed_task(title)
+  Task.find_or_create_by(title: title)
+  dot
+end
+
+seed_task 'Eat'
+seed_task 'Sleep'
+seed_task 'Repeat'
